f78c85ffbf012442b11c86f277e94f0f660b631f
kimggor <dkdjsema9808@naver.com>
2024-07-28 21:29:15 +0900

    search연결 및 스타일링

--- js/index.js
+++ js/index.js
@@ -5,3 +5,30 @@ btnTop.addEventListener("click", function () {
     top: 0,
   });
 });
+
+document.getElementById("searchBtn").addEventListener("click", function () {
+  document.body.classList.add("overlay-visible");
+  document.getElementById("search-overlay").classList.remove("hidden");
+});
+
+document.querySelector(".search_delete").addEventListener("click", function () {
+  clearSearch();
+});
+document.querySelector(".close-overlay").addEventListener("click", function () {
+  clearSearch();
+});
+function clearSearch() {
+  document.getElementById("search").value = "";
+  document.body.classList.remove("overlay-visible");
+  document.getElementById("search-overlay").classList.add("hidden");
+}
+
+document.getElementById("search").addEventListener("input", function () {
+  if (this.value.length > 0) {
+    document.querySelector("main").style.display = "none"; // 메인 콘텐츠 숨기기
+    document.querySelector(".search_delete").style.display = "flex"; // 삭제 버튼 표시
+  } else {
+    document.querySelector("main").style.display = "block"; // 메인 콘텐츠 표시
+    document.querySelector(".search_delete").style.display = "none"; // 삭제 버튼 숨기기
+  }
+});
